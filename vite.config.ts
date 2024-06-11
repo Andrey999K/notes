f29bfab8d8db830e256d8db6882d5a234e09e12b
Andrey999K <andrey1234xxxx@mail.ru>
2024-06-11 23:08:50 +0300

    build: add prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
+import path from "path";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@img": path.resolve(__dirname, "./public/images"),
+    },
+  },
+  server: {
+    open: true,
+    port: 3030,
+  },
 })
